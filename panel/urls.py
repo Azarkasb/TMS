8890562d5cdb836ead12f358239cbea2a6dd29d0
Ali Az <azarkasb.ali@gmail.com>
2023-01-22 20:26:01 +0330

    complete my tasks section for employers

--- panel/urls.py
+++ panel/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from panel import views
 
+app_name = 'panel'
 urlpatterns = [
     path("", views.EmployerPanel.as_view(), name='employer_panel')
 ]
--- panel/views.py
+++ panel/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
+from tasks.models import Task, Employer
+
 
 class EmployerPanel(TemplateView):
     template_name = 'employer_tasks.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        employer = Employer.objects.get(user=self.request.user)
+        context['pending_tasks'] = Task.objects.filter(owner=employer, state='P')
+        context['assigned_tasks'] = Task.objects.filter(owner=employer, state='A')
+        context['done_tasks'] = Task.objects.filter(owner=employer, state='D')
         return context
